f71b11fb213d7cc3c391cf9f442e90011829dcff
Fukuda097548205980248520795274 <k024g0083@g.neec.ac.jp>
2025-04-08 18:21:45 +0900

    ベクトルの計算と出力を作成した

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <Novice.h>
+#include "Struct.h"
+#include "./Func/Vector/Vector.h"
 
 const char kWindowTitle[] = "LE2A_11_フクダソウワ_MT3";
 
@@ -12,6 +14,28 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+
+	/*---------------
+	    変数を作る
+	---------------*/
+
+	// ベクトル
+	Vector3 v1 = { 1.0f , 3.0f , -5.0f };
+	Vector3 v2 = { 4.0f , -1.0f , 2.0f };
+
+	// スカラー
+	float k = 4.0f;
+
+
+	// 計算結果
+	Vector3 resultAdd = Add(v1, v2);
+	Vector3 resultSubtract = Subtract(v1, v2);
+	Vector3 resultMultiply = Multiply(k, v1);
+	float resultDot = Dot(v1, v2);
+	float resultLength = Length(v1);
+	Vector3 resultNormalize = Normalize(v2);
+
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -33,6 +57,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		///
 		/// ↓描画処理ここから
 		///
+
+		VectorScreenPrintf(0, 0, resultAdd, " : Add");
+		VectorScreenPrintf(0, kRowHeight, resultSubtract, " : Subtract");
+		VectorScreenPrintf(0, kRowHeight * 2, resultMultiply, " : Multiply");
+		Novice::ScreenPrintf(0, kRowHeight * 3, "%.02f : Dot", resultDot);
+		Novice::ScreenPrintf(0, kRowHeight * 4, "%.02f : Length", resultLength);
+		VectorScreenPrintf(0, kRowHeight * 5, resultNormalize, " : Normalize");
 
 		///
 		/// ↑描画処理ここまで
